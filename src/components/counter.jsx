338191eda92021a6629254ccf6c54d14a29f69bf
smudgegame <atmargraff@gmail.com>
2020-05-05 21:29:06 -0400

    Add nav bar and lift methods to App

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
   styles = {
     fontSize: 14,
     fontWeight: "bold",
-  };
-
-  handleIncrement = (product) => {
-    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -21,7 +13,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={(product) => this.handleIncrement(product)}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           style={this.styles}
           className="btn btn-secondary btn-sm"
         >
@@ -39,13 +31,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
